fdbdc8bc34cd7670c0558e74f2a364dfdc77a484
unknown <rohanbhomia1002@gmail.com>
2024-06-09 21:10:05 +0530

    .

--- app_books/views.py
+++ app_books/views.py
@@ -1,12 +1,23 @@
+from typing import Any
 from rest_framework.views import APIView
-from app_excel_generate.views import generate_excel
+from app_excel_generate.views import GenerateExcel
 from .models import Book
 
 
-class BookExcel(APIView):
+class BookExcel(APIView, GenerateExcel):
+
+    def __init__(self, **kwargs: Any) -> None:
+        self.rename_columns = {'title': 'Title', 'author': 'Author', 'publication_date': 'Date', 'isbn': 'ISBN'}
+        self.color_columns = [
+            {
+               'column': 'Title',
+               'color': 'FFC7CE',
+               'index': 'A1'
+            }
+        ]
 
     def get(self, request):
         obj = Book.objects.values('title','author','publication_date','isbn')
-        response = generate_excel(obj)
+        response = self.generate_excel(obj)
         print(response)
         return response
--- app_excel_generate/views.py
+++ app_excel_generate/views.py
@@ -2,14 +2,23 @@ import pandas as pd
 from django.http import HttpResponse
 import io
 
+class GenerateExcel():
 
-def generate_excel(data):
-    df = pd.DataFrame(data)
-    excel_buffer = io.BytesIO()
-    excel_writer = pd.ExcelWriter(excel_buffer, engine='xlsxwriter')
-    df.to_excel(excel_writer, index=False)
-    excel_writer.close()
-    excel_buffer.seek(0)
-    response = HttpResponse(excel_buffer.getvalue(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    response['Content-Disposition'] = 'attachment; filename=my_excel_file.xlsx'
-    return response
+    def generate_excel(self, data):
+        df = pd.DataFrame(data)
+        if self.rename_columns:
+            df = df.rename(columns=self.rename_columns)
+        excel_buffer = io.BytesIO()
+        excel_writer = pd.ExcelWriter(excel_buffer, engine='xlsxwriter')
+        df.to_excel(excel_writer, index=False)
+        if self.color_columns:
+            workbook = excel_writer.book
+            worksheet = excel_writer.sheets['Sheet1']
+            for cols in self.color_columns:
+                cell_format = workbook.add_format({'bg_color': cols.get('color'),'bold': True})  # Red background color
+                worksheet.write(cols.get('index'),cols.get('column'), cell_format)
+        excel_writer.close()
+        excel_buffer.seek(0)
+        response = HttpResponse(excel_buffer.getvalue(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=my_excel_file.xlsx'
+        return response
